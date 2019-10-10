ed80072cd5a253c0715bccb6a62db7a444491a23
XHaotaX <31614897+XHaotaX@users.noreply.github.com>
2019-10-10 11:58:58 +0300

    Add files via upload
    исправлено победитель в случае одинаковых карт

--- ranf.py
+++ ranf.py
@@ -34,52 +34,63 @@ def compare(Game,pl1,pl2):
             continue
         else:
             if hand1[i][1]>hand2[i][1]:
-                return 0
+                return -1
             else:
                 return 1
     k1=ch.CutAnd(PLC1,hand1)
     k2=ch.CutAnd(PLC2,hand2)
-##    print(k1,k2)
+    print(k1,k2)
     for i in range(len(k1)):
         if k2[i][1]==k1[i][1]:
             continue
         else:
             if k1[i][1]>k2[i][1]:
-                return 0
+                return -1
             else:
                 return 1
+    return 0
 
 com=["fleshRoal","stritFlesh","Kare","FullHouse",
      "Flush","Straight","Set","2pair","pair","Kiker"]
-gg=[[2, 2], [2, 3], [2, 5], [2, 10], [3, 8], [3, 1], [4, 4],[1,4],[3,6]]
+gg=[[2, 2], [2, 3], [1, 5], [2, 10], [3, 8], [1,4],[2,4],[3, 4], [4, 4],[1,7],[2,1]]
 combHand=[]
 for i in range(1):
-    allcards=gg
-##    NPlaer=2
-##    comb=gener.gg(NPlaer)#по умолчанияю 2
-##    allcards=gener.gg(2)
+##    allcards=gg
+    NPlaer=3
+    allcards=gener.gg(NPlaer)
     curGame=table()
-    curGame.com=allcards[:5]
-    curGame.winner=10,10
+    curGame.com=allcards[:5]##последние 5 карт из сгенрированой группы обшие 
+    curGame.winner.append([10,10,10])
     print(curGame.com)
     for i in range(int((len(allcards)-5)/2)):
-        curGame.Plaers.append(allcards[5+2*i:5+2*(i+1)])
-##    theHighest(comb,combHand)
-        PLC=curGame.com+curGame.Plaers[i]
+        curGame.Plaers.append(allcards[5+2*i:5+2*(i+1)])##получаю карты игрока
+        ##    theHighest(comb,combHand)
+        PLC=curGame.com+curGame.Plaers[i]##формирую массив всех карт игрока (рука+стол)
         print("Plyer "+str(i+1)+" -",curGame.Plaers[i])
-        number,combHand=ch.pure(PLC,combHand)
+        number,combHand=ch.pure(PLC,combHand)##вывод исхода карт для игрока, number-номер комбинации combHand-карты комбинации
         print("-",com[number],combHand)
-        if curGame.winner[0]>number:
-            curGame.winner=number,i,combHand
-        else:  
-            if curGame.winner[0]==number:
-                if compare(curGame,curGame.winner[1],i):
-                    curGame.winner=number,i,combHand
-    
-    print("Player ",curGame.winner[1]+1,"-",com[curGame.winner[0]])
+
+        if curGame.winner[0][0]>number:
+            curGame.winner.clear()
+            curGame.winner.append([number,i,combHand.copy()])
+        else:
+            if curGame.winner[0][0]==number:
+                temp=compare(curGame,curGame.winner[0][1],i)
+                if temp==1:
+                    curGame.winner.clear()
+                    curGame.winner.append([number,i,combHand.copy()])
+                else:
+                    if temp==0:
+                        curGame.winner.append([number,i,combHand.copy()])
+
+    if (len(curGame.winner)>1):
+        print("Plaers делят банк")
+        for i in range(len(curGame.winner)):
+            print("Player ",curGame.winner[i][1]+1,"-",com[curGame.winner[i][0]])
+    print("Player ",curGame.winner[0][1]+1,"-",com[curGame.winner[0][0]])
+
 ##        if number>2 and number<9:## вывод кикера с учетом текуших карт
 ##            print("kKk-",ch.CutAnd(PLC,combHand))
-
 # стрит флешь старшая карта
 # каре старшая карта
 # фулл хаус старшая среди троек , старшая среди 2, кикер
