c72dd7c91469b402cfe7bde2aefd170aa3757b17
XHaotaX <31614897+XHaotaX@users.noreply.github.com>
2019-11-22 15:45:34 +0300

    add def gRound
    and class(canser class) Plaer

--- ranf.py
+++ ranf.py
@@ -12,8 +12,16 @@ class table:
     Plaers=[]
     winner=[]
 
+class Plaer:
+##    позиция за столом 
+    position=[]
+##  карты игрока  
+    card=[]
+##  состояние пока не продуман?( , не участвует в раздаче ,не участвует в розыгрыше банка и .?. 
+    state=[]
+
 def compare(Game,pl1,pl2):
-    print(pl1,pl2)
+##    print(pl1,pl2)
     PLC1=Game.com+Game.Plaers[pl1]
     PLC2=Game.com+Game.Plaers[pl2]
     hand1=[]
@@ -39,7 +47,7 @@ def compare(Game,pl1,pl2):
                 return 1
     k1=ch.CutAnd(PLC1,hand1)
     k2=ch.CutAnd(PLC2,hand2)
-    print(k1,k2)
+#    print(k1,k2)
     for i in range(len(k1)):
         if k2[i][1]==k1[i][1]:
             continue
@@ -50,51 +58,80 @@ def compare(Game,pl1,pl2):
                 return 1
     return 0
 
-com=["fleshRoal","stritFlesh","Kare","FullHouse",
-     "Flush","Straight","Set","2pair","pair","Kiker"]
-gg=[[2, 2], [2, 3], [1, 5], [2, 10], [3, 8], [1,4],[2,4],[3, 4], [4, 4],[1,7],[2,1]]
-combHand=[]
-for i in range(1):
-##    allcards=gg
-    NPlaer=3
-    allcards=gener.gg(NPlaer)
-    curGame=table()
-    curGame.com=allcards[:5]##последние 5 карт из сгенрированой группы обшие 
-    curGame.winner.append([10,10,10])
-    print(curGame.com)
+def gRound(allcards):
+    Plaers=Plaer()
     for i in range(int((len(allcards)-5)/2)):
-        curGame.Plaers.append(allcards[5+2*i:5+2*(i+1)])##получаю карты игрока
-        ##    theHighest(comb,combHand)
-        PLC=curGame.com+curGame.Plaers[i]##формирую массив всех карт игрока (рука+стол)
-        print("Plyer "+str(i+1)+" -",curGame.Plaers[i])
-        number,combHand=ch.pure(PLC,combHand)##вывод исхода карт для игрока, number-номер комбинации combHand-карты комбинации
-        print("-",com[number],combHand)
+        ## вот как сука сделать temp=Plaer() который не будет привязан
+        ## rft;t 'nj ujdyj
+##        temp=Plaer()
+##        curGame.Plaers.append(allcards[5+2*i:5+2*(i+1)])##получаю карты игрока
+##        temp.card.append(allcards[5+2*i:5+2*(i+1)])
+##        temp.position=i
+##        Plaers.append(copy(temp))
+        Plaers.card.append(allcards[5+2*i:5+2*(i+1)])
+        Plaers.position.append(i)
+    for i in range(len(Plaers.card)):
+        print ("Your cards is : ",Plaers.card[i],Plaers.position[i])
+##    =input()
+    print(o+2)
 
-        if curGame.winner[0][0]>number:
-            curGame.winner.clear()
-            curGame.winner.append([number,i,combHand.copy()])
-        else:
-            if curGame.winner[0][0]==number:
-                temp=compare(curGame,curGame.winner[0][1],i)
-                if temp==1:
-                    curGame.winner.clear()
-                    curGame.winner.append([number,i,combHand.copy()])
-                else:
-                    if temp==0:
+    ## call(уровнять), rais on {n}(), allin (поставить , но это врядли буду реализовывать хотя нет придется( )
+## chech(оставить ставку без повышения), fold(сбросить карты, и выйти из текушей игры(не стол))
+## leav покинуть стол но до этого ещё далеко ) (
+    if 
+
+def main():
+    com=["fleshRoal","stritFlesh","Kare","FullHouse",
+         "Flush","Straight","Set","2pair","pair","Kiker"]
+    gg=[[2, 2], [2, 3], [1, 5], [2, 10], [3, 8], [1,4],[2,4],[3, 4], [4, 4],[1,7],[2,1]]
+    combHand=[]
+    for i in range(1):
+    ##    allcards=gg
+        NPlaer=3
+        allcards=gener.gg(NPlaer)
+        curGame=table()
+        curGame.com=allcards[:5]##5 карт из сгенрированой группы обшие 
+        curGame.winner.append([10,10,10])
+        print(curGame.com)
+        gRound(allcards)
+        for i in range(int((len(allcards)-5)/2)):
+            curGame.Plaers.append(allcards[5+2*i:5+2*(i+1)])##получаю карты игрока
+            ##    theHighest(comb,combHand)
+            PLC=curGame.com+curGame.Plaers[i]##формирую массив всех карт игрока (рука+стол)
+##            print("Plyer "+str(i+1)+" -",curGame.Plaers[i])
+            number,combHand=ch.pure(PLC,combHand)##вывод исхода карт для игрока, number-номер комбинации combHand-карты комбинации
+            print("-",com[number],combHand)
+
+            if curGame.winner[0][0]>number:
+                curGame.winner.clear()
+                curGame.winner.append([number,i,combHand.copy()])
+            else:
+                if curGame.winner[0][0]==number:
+                    temp=compare(curGame,curGame.winner[0][1],i)
+                    if temp==1:
+                        curGame.winner.clear()
                         curGame.winner.append([number,i,combHand.copy()])
+                    else:
+                        if temp==0:
+                            curGame.winner.append([number,i,combHand.copy()])
 
-    if (len(curGame.winner)>1):
-        print("Plaers делят банк")
-        for i in range(len(curGame.winner)):
-            print("Player ",curGame.winner[i][1]+1,"-",com[curGame.winner[i][0]])
-    print("Player ",curGame.winner[0][1]+1,"-",com[curGame.winner[0][0]])
+        if (len(curGame.winner)>1):
+            print("Plaers делят банк")
+            for i in range(len(curGame.winner)):
+                print("Player ",curGame.winner[i][1]+1,"-",com[curGame.winner[i][0]])
+        else :
+            print("Player ",curGame.winner[0][1]+1,"-",com[curGame.winner[0][0]])
 
-##        if number>2 and number<9:## вывод кикера с учетом текуших карт
-##            print("kKk-",ch.CutAnd(PLC,combHand))
-# стрит флешь старшая карта
-# каре старшая карта
-# фулл хаус старшая среди троек , старшая среди 2, кикер
-# флешь старшая карта,kiker
-# стрит старшая карта,kiker
-# тройка старшая карта, кикер
-# пара старшая карта, кикер
+    ##        if number>2 and number<9:## вывод кикера с учетом текуших карт
+    ##            print("kKk-",ch.CutAnd(PLC,combHand))
+    # стрит флешь старшая карта
+    # каре старшая карта
+    # фулл хаус старшая среди троек , старшая среди 2, кикер
+    # флешь старшая карта,kiker
+    # стрит старшая карта,kiker
+    # тройка старшая карта, кикер
+    # пара старшая карта, кикер
+
+
+    
+main()
