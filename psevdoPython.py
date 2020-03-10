733cd16b1362e971428cd28778436be4fcf4c4ca
XHaotaX <31614897+XHaotaX@users.noreply.github.com>
2020-03-10 13:35:37 +0300

    done
    рабочие правила, вроде
    осталась прикрутить к основному файлу ranf

--- psevdoPython.py
+++ psevdoPython.py
@@ -1,56 +1,145 @@
 from collections import deque
-
-class Plaer:
+##27.02.2020 09:55 как же сдесь мало кометраиев
+## нужно больше крови
+class Player:
     card=[]
     state=0
     bet=0
     id
     
 def main():
-   
+    gg=[[2, 12], [1, 5], [3, 12], [4, 12], [1, 12], [2, 8], [1, 2], [4, 5], [2, 9], [2, 7], [2, 4]]   
+    around=0
     bank=0
-    or
-    bakr=bb+mbb
+    bb=2
+##    or
+##    bakr=bb+mbb
     curCost=0
-    endRoundId=n
+    endRoundId=int((len(gg)-5)/2)
     Pls=deque()
     
-    for i in nP##тут
-        pl=Plaer()##типо заполняю 
-        pl.card.append(gg[5+n+(1-2)])##даю карты каждому игроку
-        pl.id=i
-        Pls.append(pl)
-        
-    ##теперь есть масив обьектов представляюших каждого игрока Pls
-
-    Pls[0].bet=bb
-    Pls[1].bet=bb/2
-    
-    for pl in Pls
-    
-        if endRoundId==pl.id:##тут типо оканчание круга торговли,
-            ## переход на следуюшую улицу или вообше выход и там покмане придумал            
-            return 1
-
-        if Pls.state=0
-            continue
-        move=0
+    for i in range(int((len(gg)-5)/2)):##тут
+##        pl=Player()##типо заполняю 
+##        pl.card.append(gg[5+2*i:5+2*(i+1)])##даю карты каждому игроку
+##        pl.id=i
+##        Pls.append(pl)
+        Pls.append(Player())
+        Pls[i].card=gg[5+2*i:5+2*(i+1)]
+##        Pls.##даю карты каждому игроку
+        Pls[i].id=i
+        Pls[i].state=9
+    for pl in Pls:
         print(pl.card)
-        move=input()
-        if move=0: ##fall
-            pl.state=0
+        print(pl.id)
+    end=0
+    table=[]
+    ##теперь есть масив обьектов представляюших каждого игрока Pls
+    curCost=bb
+    Pls[0].bet=bb/2
+    Pls.rotate(-1)
+    Pls[0].bet=bb
+    endRoundId=Pls[0].id
+    print("start")
+    while True:
+        Pls.rotate(-1)
+        print("id-",Pls[0].id)
+        if endRoundId==Pls[0].id:##тут типо оканчание круга торговли,
+            around=around+1
+            if around==1:
+                print(gg[:3])
+                table=gg[:3]
+            if around==2:
+                print(gg[:4])
+                table=gg[:4]
+            if around==3:
+                print(gg[:5])
+                table=gg[:5]
+            if around==4:
+                break##конец торговли
+ #            0- 0 карт на столе
+##            1- 3 карты на столе
+##            2- 4 карты на столе
+##            3- 5 карт на столе
+            ## переход на следуюшую улицу или вообше выход и там покмане придумал
+            ## а там просто сравнение карта тоесть алогритм делает точто уже умеет
+            ## смотреть затем чтоб непроверял игроков не принемашив участиев розыгрыше, и ли по иным причинам
+            ##не имеющий прав участвовать в розыгреше
+            
+        for pl in Pls:
+            if pl.state==0:
+                end=end+1    
+            if (end+1)==int((len(gg)-5)/2):
+                return ##конец торговли
+        end=0
+##  навыводить всю информацию каждому игроку о  текушем столе
+        if not (Pls[0].state==0 or Pls[0].state==4):
+            print(table)
+            print("id \t state \t bet")
+            for pl in Pls:
+                print(pl.id,"\t",pl.state,"\t",pl.bet,"$")
+        if Pls[0].state==0 or Pls[0].state==4:
             continue
-        if move=1:
-            pl.state=1
-            continue
-        if move=2:
-            bank=bank+(curCost-pl.bet)
-            pl.bet=curCost
-            pl.state=2 ##2 or 1 thinking about it
-            continue
-        if move=3:##rais фунциюю all in можно сделать также
-            bet=input()##ток добавь к параметрам класса парметр текушихших вишек
-            bank=bank+bet
-            curCost=curCost+bet
-            pl.bet=pl.bet+bet##сделано одельно чтоб в случии ошибки были видны @)
-            endRoundId=pl.id         
+        else:
+            move=0
+            print(Pls[0].card)
+            if curCost==Pls[0].bet:
+                print(" fall \t check \t raise")
+                move=input()
+                if move=="f": ##fall
+                    Pls[0].state=0
+                    continue
+                if move=="ch":
+                    Pls[0].state=1
+                    continue
+                if move=="r":##rais фунциюю all in можно сделать также
+                    while True:
+                        state=3
+                        bet=input("You is push r.\nBB bet pls:")##ток добавь к параметрам класса парметр текушихших вишек
+                        if "all"==bet:
+                            print("all")
+                            Pls[0].state=4
+                            bet=100
+                            break
+                        if str.isdigit(bet):
+                            bet=int(bet)
+                            break
+                        else:
+                           print("non number")
+                    bank=bank+bet
+                    curCost=curCost+bet
+                    Pls[0].bet=Pls[0].bet+bet##сделано одельно, чтоб в случии, ошибки были видны @)
+            else:
+                print(" fall \t call  \t raise")
+                move=input()
+                if move=="f": ##fall
+                    Pls[0].state=0
+                    continue
+                if move=="c":
+                    bank=bank+(curCost-Pls[0].bet)##call
+                    Pls[0].bet=curCost
+                    Pls[0].state=2 ##2 or 1 thinking about it
+                    continue
+                if move=="r":##rais фунциюю all in можно сделать также
+                    while True:
+                        state=3
+                        bet=input()##ток добавь к параметрам класса парметр текушихших вишек
+                        if "all"==bet:
+                            print("all")
+                            Pls[0].state=4
+                            bet=100
+                            break
+                        if str.isdigit(bet):
+                            bet=int(bet)
+                            break
+                        else:
+                            print("non number")
+                    bank=bank+bet
+                    if bet+Pls[0].bet>curCost:
+                        curCost=bet+Pls[0].bet
+                    Pls[0].bet=Pls[0].bet+bet##сделано одельно, чтоб в случии, ошибки были видны @)
+                    endRoundId=Pls[0].id
+    for pl in Pls:
+        if not pl.state==0:
+            print(pl.id,"\t",pl.state,"\t",pl.bet,"$")
+
+main()
